51c2debfd3cdb870305eeb86ebb09b63253fdcbb
edadic <dadiceldar@gmail.com>
2025-04-24 21:58:25 +0200

    feat: add public access to booked slots
    Allow calendar to display booked appointments to users when booking appointments

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getCurrentUser } = require('../controllers/userController');
+const { getCurrentUser, updateUser } = require('../controllers/userController');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/me', protect, getCurrentUser);
+router.put('/update', protect, updateUser);
 
 module.exports = router;
